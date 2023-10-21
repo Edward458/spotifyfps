7425886a5eac300f0338bae4a5b9c094b38590c1
Edward458 <60199491+Edward458@users.noreply.github.com>
2023-10-21 18:02:00 -0400

    Added Files

---
+++ main.js
@@ -0,0 +1,9 @@
+// initializing game window
+const canvas = document.getElementById("main-window");
+
+// styling game window
+canvas.style.height = ""
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 150, 75);
